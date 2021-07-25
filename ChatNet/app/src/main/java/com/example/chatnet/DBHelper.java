68e308863a9d9b95d7409f58ba45c95fb5c7a7c5
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2021-07-25 20:10:57 +0600

    some bug fixes

--- ChatNet/app/src/main/java/com/example/chatnet/DBHelper.java
+++ ChatNet/app/src/main/java/com/example/chatnet/DBHelper.java
@@ -47,15 +47,4 @@ public class DBHelper extends SQLiteOpenHelper {
             return false;
         }
     }
-
-    public Boolean checkusernamepassword(String username, String password){
-        SQLiteDatabase MyDB = this.getWritableDatabase();
-        Cursor cursor = MyDB.rawQuery("Select * from users where username = ? and password = ?", new String[]{username, password});
-        if (cursor.getCount() > 0){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 }
